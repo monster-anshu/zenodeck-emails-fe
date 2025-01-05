a514579fd6142dff5e63b5cebe79921b46cc602c
Himanshu Gunwant <hgunwant2312@gmail.com>
2025-01-05 15:41:06 +0530

    add prettier

---
+++ packages/config-prettier/index.js
@@ -0,0 +1,23 @@
+/** @type {import("prettier").Config} */
+const config = {
+  arrowParens: "always",
+  bracketSameLine: false,
+  bracketSpacing: true,
+  cursorOffset: -1,
+  embeddedLanguageFormatting: "auto",
+  endOfLine: "lf",
+  htmlWhitespaceSensitivity: "strict",
+  insertPragma: false,
+  jsxSingleQuote: false,
+  organizeImportsSkipDestructiveCodeActions: true,
+  plugins: ["prettier-plugin-tailwindcss", "prettier-plugin-organize-imports"],
+  printWidth: 80,
+  quoteProps: "as-needed",
+  semi: true,
+  singleQuote: false,
+  tabWidth: 2,
+  trailingComma: "es5",
+  useTabs: false,
+};
+
+export default config;
