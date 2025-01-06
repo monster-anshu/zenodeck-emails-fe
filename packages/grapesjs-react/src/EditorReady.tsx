478646f7c691c1946350334e3c198f4507d52314
Himanshu Gunwant <hgunwant2312@gmail.com>
2025-01-06 13:06:02 +0530

    add grapesjs-react

---
+++ packages/grapesjs-react/src/EditorReady.tsx
@@ -0,0 +1,10 @@
+import { memo, useEffect } from "react";
+import { useEditorOptions } from "./context/EditorOptions";
+
+const EditorReady = memo(function () {
+  const options = useEditorOptions();
+  useEffect(() => options.setReady(true), []);
+  return <></>;
+});
+
+export default EditorReady;
