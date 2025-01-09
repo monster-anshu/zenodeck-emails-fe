0cedf49d5859deb57fcb52ab4a3bd4b84275bc85
Himanshu Gunwant <hgunwant2312@gmail.com>
2025-01-09 14:58:56 +0530

    editor in progress

--- packages/email-editor/src/components/RightBar.tsx
+++ packages/email-editor/src/components/RightBar.tsx
@@ -143,7 +143,7 @@ const RightBar: FC<IRightBarProps> = () => {
     <>
       {isPreview && <div className="row-span-2"></div>}
       <div
-        className="bg-background relative row-span-2 w-96 overflow-auto rounded"
+        className="bg-background relative row-span-2 w-72 overflow-auto rounded"
         style={{
           display: isPreview || !editor ? "none" : "block",
         }}
@@ -155,7 +155,7 @@ const RightBar: FC<IRightBarProps> = () => {
             if (!tab) return;
             handleTabClick(tab);
           }}
-          className="px-2 py-2"
+          className="bg-background sticky top-0 z-50 px-2 py-2"
         >
           <TabsList className="w-full border">
             {tabs.map((tab) => {
