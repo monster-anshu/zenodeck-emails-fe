f4ce0f744d4910a07cf67063c53941c1c4ae76db
Himanshu Gunwant <hgunwant2312@gmail.com>
2025-01-30 21:35:40 +0530

    feat: add login path on 401 redirect

--- apps/web/app/services/interspectors.ts
+++ apps/web/app/services/interspectors.ts
@@ -26,7 +26,7 @@ export const commonErrorAuthInterceptor = async (
   }
 
   if (status === 401) {
-    const url = new URL(USER_SERVICE_URL);
+    const url = new URL(USER_SERVICE_URL + "/login");
     url.searchParams.set("productId", "CAMPAIGN");
     url.searchParams.set("location", encodeURIComponent(window.location.href));
     window.location.href = url.toString();
