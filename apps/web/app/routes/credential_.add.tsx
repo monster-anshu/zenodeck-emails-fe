be2d5338eb07ad73db46192ece8e0ea4a1b1eefb
monster-anshu <hgunwant2312@gmail.com>
2025-02-04 20:02:03 +0530

    remove credential add page

--- apps/web/app/routes/credential_.add.tsx
+++
@@ -1,9 +0,0 @@
-import React, { FC } from "react";
-
-type ICredentialAddPageProps = {};
-
-const CredentialAddPage: FC<ICredentialAddPageProps> = () => {
-  return <div>CredentialAddPage</div>;
-};
-
-export default CredentialAddPage;
