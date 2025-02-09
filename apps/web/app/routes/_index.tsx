592cd67163d6aa15edf9f691428aa3b2da0a7fb0
Himanshu Gunwant <hgunwant2312@gmail.com>
2025-02-09 16:48:43 +0530

    feat: add dashboard data

--- apps/web/app/routes/_index.tsx
+++ apps/web/app/routes/_index.tsx
@@ -9,6 +9,7 @@ import {
   ResponsiveContainer,
   Tooltip,
   XAxis,
+  YAxis,
 } from "recharts";
 import type { Route } from "./+types/_index";
 
@@ -20,16 +21,9 @@ export default function IndexPage() {
   const dashboardQuery = dashboardQueryOptions();
   const { data } = useQuery(dashboardQuery);
 
-  const lineChartData = Array(30)
-    .fill(null)
-    .map((_) => {
-      return {
-        name: "Page A",
-        uv: Math.random().toFixed(2),
-        pv: Math.random().toFixed(2),
-        amt: 2400,
-      };
-    });
+  const historyChart = data?.history.stats || [];
+  const ctrChart = data?.ctr.stats || [];
+  const mailOpenedChart = data?.openedMail.stats || [];
 
   return (
     <main
@@ -41,7 +35,7 @@ export default function IndexPage() {
       <DashboardCard>
         <p className="font-medium">Credentials</p>
         <p className="flex items-center justify-between text-sm">
-          <span>{4}</span>
+          <span>{data?.credential.total}</span>
           <Link to={"/credential"} aria-label="Add credential">
             {" "}
             +
@@ -50,15 +44,15 @@ export default function IndexPage() {
       </DashboardCard>
       <DashboardCard>
         <p className="font-medium">Email sent</p>
-        <p className="text-sm">{35}</p>
+        <p className="text-sm">{data?.history.total}</p>
       </DashboardCard>
       <DashboardCard>
         <p className="font-medium">CTR</p>
-        <p className="text-sm">3746</p>
+        <p className="text-sm">{data?.ctr.total}</p>
       </DashboardCard>
       <DashboardCard>
         <p className="font-medium">Mail seen</p>
-        <p className="text-sm">31</p>
+        <p className="text-sm">{data?.openedMail.total}</p>
       </DashboardCard>
       <DashboardCard>
         <p className="font-medium">Storage uses</p>
@@ -67,36 +61,36 @@ export default function IndexPage() {
       <DashboardCard className="col-span-full grid h-72 grid-rows-[auto_1fr]">
         <p className="mb-4 text-lg font-medium">Email sent</p>
         <ResponsiveContainer height={"100%"} width={"100%"}>
-          <LineChart data={lineChartData}>
-            <Line type="monotone" dataKey="uv" stroke="#8884d8" />
+          <LineChart data={historyChart}>
+            <Line type="monotone" dataKey="count" stroke="#8884d8" />
+            <XAxis dataKey="date" />
+            {/* <YAxis dataKey="count" /> */}
             <CartesianGrid strokeDasharray="3 3" className="opacity-20" />
             <Tooltip wrapperClassName="!bg-background rounded !border-border" />
-            <XAxis dataKey="pv" />
-            {/* <YAxis dataKey="uv" /> */}
           </LineChart>
         </ResponsiveContainer>
       </DashboardCard>
       <DashboardCard className="col-span-full grid h-72 grid-rows-[auto_1fr]">
         <p className="mb-4 text-lg font-medium">Email seen</p>
         <ResponsiveContainer height={"100%"} width={"100%"}>
-          <LineChart data={lineChartData}>
-            <Line type="monotone" dataKey="uv" stroke="#8884d8" />
+          <LineChart data={mailOpenedChart}>
+            <Line type="monotone" dataKey="count" stroke="#8884d8" />
+            <XAxis dataKey="date" />
+            {/* <YAxis dataKey="count" /> */}
             <CartesianGrid strokeDasharray="3 3" className="opacity-20" />
             <Tooltip wrapperClassName="!bg-background rounded !border-border" />
-            <XAxis dataKey="pv" />
-            {/* <YAxis dataKey="uv" /> */}
           </LineChart>
         </ResponsiveContainer>
       </DashboardCard>
       <DashboardCard className="col-span-full grid h-72 grid-rows-[auto_1fr]">
         <p className="mb-4 text-lg font-medium">Click rate (CTR)</p>
         <ResponsiveContainer height={"100%"} width={"100%"}>
-          <LineChart data={lineChartData}>
-            <Line type="monotone" dataKey="uv" stroke="#8884d8" />
+          <LineChart data={ctrChart}>
+            <Line type="monotone" dataKey="count" stroke="#8884d8" />
+            <XAxis dataKey="date" />
+            {/* <YAxis dataKey="count" /> */}
             <CartesianGrid strokeDasharray="3 3" className="opacity-20" />
             <Tooltip wrapperClassName="!bg-background rounded !border-border" />
-            <XAxis dataKey="pv" />
-            {/* <YAxis dataKey="uv" /> */}
           </LineChart>
         </ResponsiveContainer>
       </DashboardCard>
