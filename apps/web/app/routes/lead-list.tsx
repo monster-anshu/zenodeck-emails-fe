e7094ab25ad5363320c56c1618f344fb55940857
Himanshu Gunwant <hgunwant2312@gmail.com>
2025-02-14 11:24:30 +0000

    feat: seprate import leas component

--- apps/web/app/routes/lead-list.tsx
+++ apps/web/app/routes/lead-list.tsx
@@ -6,47 +6,26 @@ import {
   DialogHeader,
   DialogTitle,
 } from "@repo/ui/components/dialog";
-import { Dropzone } from "@repo/ui/components/dropzone";
-import { Form, FormField, FormItem, FormLabel } from "@repo/ui/components/form";
-import { Label } from "@repo/ui/components/label";
-
-import { Select } from "@repo/ui/components/react-select";
+import { Form } from "@repo/ui/components/form";
 import { FormComponent } from "@repo/ui/molecules/form-component";
 import { useMutation } from "@tanstack/react-query";
+import ImportLeads, {
+  IImportLeadsRef,
+} from "@web-components/lead-list/ImportLeads";
 import { LeadListService } from "@web-services/lead-list.service";
-import Papa from "papaparse";
-import { FC, useRef, useState } from "react";
+import { FC, useRef } from "react";
 import { useForm } from "react-hook-form";
-import { FaArrowRightLong } from "react-icons/fa6";
 import { z } from "zod";
 
 type ILeadListPageProps = {};
 
-const fieldsToMap = [
-  {
-    label: "Email",
-    value: "email",
-    required: true,
-  },
-  {
-    label: "First name",
-    value: "firstName",
-  },
-  {
-    label: "Last name",
-    value: "lastName",
-  },
-];
-
 const schema = z.object({
   name: z.string().nonempty(),
-  mapping: z.record(z.string().optional().or(z.null())),
 });
 
 const LeadListPage: FC<ILeadListPageProps> = () => {
-  const [headers, setHeaders] = useState([] as string[]);
-  const [selectedFile, setSelectedFile] = useState(null as null | File);
-  const dataRef = useRef<unknown[]>([]);
+  const ref = useRef<IImportLeadsRef | null>(null);
+
   const addMutation = useMutation({
     mutationFn: LeadListService.add,
   });
@@ -55,81 +34,16 @@ const LeadListPage: FC<ILeadListPageProps> = () => {
     resolver: zodResolver(schema),
     defaultValues: {
       name: "",
-      mapping: {},
     },
   });
-  const values = form.watch();
-
-  const handleFileSelect = (file: File) => {
-    Papa.parse(file, {
-      header: true,
-      complete: function (results) {
-        setSelectedFile(file);
-        dataRef.current = results.data;
-        const headers = results.meta.fields;
-        setHeaders(headers || []);
-      },
-      error() {
-        alert("Error in csv");
-      },
-      skipEmptyLines: true,
-    });
-  };
 
   function onSubmit(values: z.infer<typeof schema>) {
-    console.log(values);
-    let hasError = false;
-    for (const field of fieldsToMap) {
-      const key = `mapping.${field.value}` as const;
-      const csvField = values.mapping[field.value];
-
-      if (field.required && !csvField) {
-        form.setError(key, {
-          message: "Required",
-          type: "required",
-        });
-        hasError = true;
-      }
-    }
-
-    if (hasError) {
+    const result = ref.current?.get();
+    if (!result) {
       return;
     }
-
-    const leads: Record<string, string>[] = [];
-
-    dataRef.current.forEach((item) => {
-      const data = item as Record<string, string>;
-      const lead = {} as Record<string, string>;
-      for (const field of fieldsToMap) {
-        const csvField = values.mapping[field.value];
-        if (!csvField) {
-          continue;
-        }
-        const value = data[csvField];
-        if (!value && field.required) {
-          return;
-        }
-        if (value) {
-          lead[field.value] = value;
-        }
-      }
-      leads.push(lead);
-    });
-
-    addMutation.mutate({ name: values.name, leads: leads });
+    addMutation.mutate({ name: values.name, leads: result.leads });
   }
-
-  const setMapping = (field: string, value: string | null) => {
-    const key = `mapping.${field}` as const;
-    form.setValue(key, value || undefined);
-    form.clearErrors(key);
-  };
-
-  const headerOptions = headers.map((header) => ({
-    label: header,
-    value: header,
-  }));
 
   return (
     <div>
@@ -152,55 +66,8 @@ const LeadListPage: FC<ILeadListPageProps> = () => {
                     placeholder: "User list",
                   }}
                 />
-                {!selectedFile && (
-                  <Dropzone
-                    onFileSelect={handleFileSelect}
-                    className="col-span-2"
-                  />
-                )}
-                {selectedFile && (
-                  <div className="col-span-2">
-                    <Label className="col-span-full">Field mapping</Label>
-                    {fieldsToMap.map((item) => {
-                      const key = `mapping.${item.value}` as const;
-                      const selectedHeader = values.mapping[item.value] || null;
 
-                      return (
-                        <FormField
-                          control={form.control}
-                          name={key}
-                          key={key}
-                          render={({ fieldState }) => (
-                            <FormItem className="grid grid-cols-[1fr_50px_1fr] items-center gap-2">
-                              <FormLabel>
-                                {item.label} {item.required && <span>*</span>}
-                              </FormLabel>
-                              <FaArrowRightLong />
-                              <Select
-                                options={headerOptions}
-                                onChange={(value) =>
-                                  setMapping(item.value, value?.value || null)
-                                }
-                                error={fieldState.error ? true : false}
-                                placeholder="Select CSV field"
-                                isClearable
-                                value={
-                                  selectedHeader
-                                    ? {
-                                        value: selectedHeader,
-                                        label: selectedHeader,
-                                      }
-                                    : null
-                                }
-                              />
-                            </FormItem>
-                          )}
-                        />
-                      );
-                    })}
-                  </div>
-                )}
-
+                <ImportLeads ref={ref} className="col-span-2" />
                 <Button
                   className="col-span-2"
                   type="submit"
